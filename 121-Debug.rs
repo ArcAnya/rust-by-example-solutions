ff42711e53f12a8fd34a386c70ad10a8b991b448
Anya <75732239+ArcAnya@users.noreply.github.com>
2022-04-03 13:22:01 +0100

    Manually formatting the Debug example

--- 121-Debug.rs
+++ 121-Debug.rs
@@ -1,1 +1,24 @@
+// need to add this line to be able to manually format with Display here
+use std::fmt::{self, Formatter, Display};
 
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+
+// Question: what's this <'_> for? 
+impl Display for Person<'_> {
+    fn fmt(&self, f:&mut Formatter) -> fmt::Result {
+        write!(f, "His name is {} and he is {} years old.", self.name, self.age)
+    }
+}
+
+fn main() {
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print
+    println!("{}", peter);
+}
