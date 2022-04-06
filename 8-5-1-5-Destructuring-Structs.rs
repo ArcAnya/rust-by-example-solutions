9342217d4c11923a0894e4fbb4f3c6737a1c6132
Anya <75732239+ArcAnya@users.noreply.github.com>
2022-04-06 11:36:37 +0100

    Slight modif to practice

--- 8-5-1-5-Destructuring-Structs.rs
+++ 8-5-1-5-Destructuring-Structs.rs
@@ -2,17 +2,18 @@ fn main() {
     struct Foo {
         x: (u32, u32),
         y: u32,
+        z: [u32; 3],
     }
 
     // Try changing the values in the struct to see what happens
-    let foo = Foo { x: (1, 2), y: 3 };
+    let foo = Foo { x: (2, 2), y: 2, z: [6, 4, 3] };
 
     match foo {
-        Foo { x: (1, b), y } => println!("First of x is 1, b = {},  y = {} ", b, y),
+        Foo { x: (1, b), y, z } => println!("First of x is 1, b = {},  y = {} and z = {:?} ", b, y, z),
 
         // you can destructure structs and rename the variables,
         // the order is not important
-        Foo { y: 2, x: i } => println!("y is 2, i = {:?}", i),
+        Foo { y: 2, x: i, z: j } => println!("y is 2, i = {:?} and my array = {:?}", i, j),
 
         // and you can also ignore some variables:
         Foo { y, .. } => println!("y = {}, we don't care about x", y),
